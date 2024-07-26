687199c417cf985e17ae0b4d1b4803e93003b7cc
hulei3 <3756313537@qq.com>
2024-07-26 11:39:56 +0800

    Initial commit
    Signed-off-by: hulei3 <3756313537@qq.com>

---
+++ 10  闭包和装饰器/5 通用的装饰器.py
@@ -0,0 +1,17 @@
+# 1.装饰带有参数的函数
+# 定义函数
+def decorator(func):
+    def inner(a,b):
+        print("正在努力执行加法运算.......")
+        num = func(a,b)
+        return num
+    return inner
+
+@decorator   # 等价于add num=decorator(add_num)
+def add_num(num1,num2):
+    result=num1+num2
+    return result
+    print(f"结果为:{result}")
+
+result1=add_num(1,2)
+print(f"结果是：{result1}")
